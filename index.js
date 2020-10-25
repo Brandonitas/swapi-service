7e1b7ed3e63d7bf48b023756b60a7ae65f72eeb2
Brandon Reyes <brandon.reyesmi@gmail.com>
2020-10-25 00:40:54 -0500

    Heroku setup bugs fixed

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const { dbConnection } = require('./database/config');
 require('dotenv').config();
+const cors = require('cors');
 
 //Crear servidor de express
 const app = express();
 
 //Conexion base de datos
 dbConnection();
+
+//CORS
+app.use(cors());
 
 //Directorio publico
 app.use(express.static('public'));
@@ -17,7 +21,7 @@ app.use(express.json());
 //Rutas
 //CRUD: Starships
 app.use('/api/starship', require('./routes/starships'));
-
+ 
 //Escuchar peticiones
 app.listen(process.env.PORT,()=>{
     console.log(`Corriendo en puerto ${process.env.PORT}`)
